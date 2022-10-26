ca6b596b4c04002809c4bd4ce1c012167ab4c537
KillianVendewinkele <killian.vendewinkele@gmail.com>
2022-10-26 16:36:56 +0200

    Déplacement de concours.dart vers le dossier view

--- app_flutter_coding/lib/main.dart
+++ app_flutter_coding/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:app_flutter_coding/connexion/login_page.dart';
 import 'package:app_flutter_coding/view/drawer.dart';
 import 'package:app_flutter_coding/view/equitation.dart';
 import 'package:app_flutter_coding/view/cours.dart';
+import 'package:app_flutter_coding/view/concours.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
