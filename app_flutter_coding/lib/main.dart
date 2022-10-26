ba122ec2e455f643412529d5df7b9794f937b35d
KillianVendewinkele <killian.vendewinkele@gmail.com>
2022-10-26 16:45:18 +0200

    Déplacement d'actu.dart dans le dossier view

--- app_flutter_coding/lib/main.dart
+++ app_flutter_coding/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:app_flutter_coding/view/drawer.dart';
 import 'package:app_flutter_coding/view/equitation.dart';
 import 'package:app_flutter_coding/view/cours.dart';
 import 'package:app_flutter_coding/view/concours.dart';
+import 'package:app_flutter_coding/view/actu.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
