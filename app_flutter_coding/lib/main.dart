993be8d14bf5f7a19ef2db5221b033a0784c83b0
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-10-26 16:22:28 +0200

    Calendrier en français + récupération de la date

--- app_flutter_coding/lib/main.dart
+++ app_flutter_coding/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:app_flutter_coding/connexion/login_page.dart';
 import 'package:app_flutter_coding/view/drawer.dart';
 import 'package:app_flutter_coding/view/equitation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
+  initializeDateFormatting().then((_) => runApp(MyApp()));
   runApp(const MyApp());
 }
 
