ad3b1c1d83255528bec51e518a5adfb9cc0d14f7
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-10-26 14:58:24 +0200

    Equitation menu

--- app_flutter_coding/lib/view/equitation.dart
+++ app_flutter_coding/lib/view/equitation.dart
@@ -13,38 +13,59 @@ class Equitation extends StatefulWidget {
 class _EquitationState extends State<Equitation> {
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20),
+      minimumSize: const Size(150, 50),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Equitation'),
       ),
       drawer: const MyDrawer(),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // create 3 buttons
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/equitation');
-              },
-              child: const Text('Equitation'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/dressage');
-              },
-              child: const Text('Dressage'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/saut');
-              },
-              child: const Text('Saut'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                width: 50,
+              ),
+              ElevatedButton(
+                style: style,
+                onPressed: () {
+                  Navigator.pushNamed(context, '/equitation');
+                },
+                child: const Text('Equitation'),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                style: style,
+                onPressed: () {
+                  Navigator.pushNamed(context, '/concours');
+                },
+                child: const Text('Concours'),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                style: style,
+                onPressed: () {
+                  Navigator.pushNamed(context, '/profil');
+                },
+                child: const Text('Soirée'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
 
