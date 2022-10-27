619bf5b47442f377edc2841dbd9d0d79593a9aa9
FlorianCohenJoly <floriancohenjoy@gmail.com>
2022-10-27 16:07:44 +0200

    update

--- app_flutter_coding/lib/connexion/inscription_page.dart
+++ app_flutter_coding/lib/connexion/inscription_page.dart
@@ -358,7 +358,7 @@ class _LoginFormState extends State<LoginForm> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => Body(),
+                      builder: (context) => MongoDbDisplay(),
                     ),
                   );
                 },
