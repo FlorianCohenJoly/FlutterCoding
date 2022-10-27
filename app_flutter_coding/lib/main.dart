38bef93b732d631e2f9fa753f1822fd719cc712e
KillianVendewinkele <killian.vendewinkele@gmail.com>
2022-10-27 16:19:20 +0200

    Page d'accueil modifiée

--- app_flutter_coding/lib/main.dart
+++ app_flutter_coding/lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: LoginPage(),
+      home: Body(),
       theme: ThemeData(
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: 
